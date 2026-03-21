e916cd3ab4c1f1e6546501f94e4d4791b5cad2d9
jjoaom <joaomarcos.aquino@hotmail.com>
2026-03-20 22:21:35 -0300

    Fix: new code directory location

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/homepage/Header";
-import Home from "./components/homepage/Home";
+import Header from "./pages/homepage/Header.jsx"
+import Home from "./pages/homepage/Home";
 //icones importados de https://react-icons.github.io/react-icons/ em cada componente segregado
 function App() {
 
