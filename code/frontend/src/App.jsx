3266a0299ee00776f590b833a0792c7747b3d9ed
jjoaom <joaomarcos.aquino@hotmail.com>
2026-04-07 18:27:07 -0300

    Add: Routes to frontend cadastro and login components

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./pages/homepage/Header.jsx"
 import Home from "./pages/homepage/Home";
+import CadastroClient from "./pages/user/CadastroClient.jsx";
+import LoginClient from "./pages/user/LoginClient.jsx";
 //icones importados de https://react-icons.github.io/react-icons/ em cada componente segregado
 function App() {
 
@@ -9,7 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/login" element={<LoginClient />} />
+        <Route path="/cadastro" element={<CadastroClient />} />
       </Routes>
     </>
   )
--- code/frontend/src/pages/homepage/Header.jsx
+++ code/frontend/src/pages/homepage/Header.jsx
@@ -3,6 +3,8 @@ import { RiHome5Line } from "react-icons/ri";
 import { IoCarSportSharp } from "react-icons/io5";
 import { GiCarKey } from "react-icons/gi";
 import { GiFullMotorcycleHelmet } from "react-icons/gi";
+import { RiLoginBoxLine } from "react-icons/ri";
+import { GoSignIn } from "react-icons/go";
 
 export default function Header() {
   return (
@@ -24,6 +26,18 @@ export default function Header() {
         <li className="nav-item">
           <NavLink to="/pedidos" className="nav-link cor_roxa">
             <GiCarKey /> Pedidos
+          </NavLink>
+        </li>
+
+        <li className="nav-item ms-auto">
+          <NavLink to="/perfil" className="nav-link cor_roxa">
+            <RiLoginBoxLine /> Login
+          </NavLink>
+        </li>
+
+        <li className="nav-item ms-auto">
+          <NavLink to="/perfil" className="nav-link cor_roxa">
+            <GoSignIn /> Cadastro
           </NavLink>
         </li>
 
